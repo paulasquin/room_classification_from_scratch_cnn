f41d8db4d1645fea12d853036a2f532d6b89ae14
Paul Asquin <paul.asquin@awabot.com>
2018-08-01 14:50:14 +0200

    Using better structure in code, print more precision and ouput labels to txt

--- dataset.py
+++ dataset.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+# This project use the structure of the cv-tricks tutorial on Image Classification :
+# http://cv-tricks.com/tensorflow-tutorial/training-convolutional-neural-network-for-image-classification/
+# The scripts have been modified by Paul Asquin for a Room Classification project based on rooms 2D Maps
+# Written by Paul Asquin - paul.asquin@gmail.com - Summer 2018
+
 import os
 import cv2
 import glob
@@ -9,70 +14,6 @@ import sys
 from tools import *
 
 np.set_printoptions(threshold=np.inf)
-
-def read_image(filename, image_size, num_channels):
-    """ Open a file, read the image and convert it to a optimum shape"""
-    if num_channels == 1:
-        image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-    else:
-        image = cv2.imread(filename)
-    if image_size != len(image):
-        image = cv2.resize(image, (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
-    image = image.astype(np.float32)
-    # image = np.multiply(image, 1.0 / 255.0)
-    image = np.where(image < 255, 0, 1)  # Improve the contrast of the dataset and transform 255 range to 0/1 values
-    return (image)
-
-
-def load_train(train_path, image_size, classes, shorter=0, num_channels=1):
-    num_images = 0
-    for fields in classes:
-        path = os.path.join(train_path, fields, '*g')
-        files = glob.glob(path)
-        if shorter == 0:
-            num_images += len(files)
-        else:
-            if len(files) < shorter:
-                num_images += len(files)
-            else:
-                num_images += shorter
-    print("num_images : " + str(num_images))
-    images = np.zeros((num_images, image_size, image_size), dtype=np.float32)
-    labels = []
-    img_names = []
-    cls = []
-    print('Going to read training images')
-    k = 0
-    for fields in classes:
-        gc.collect()
-        index = classes.index(fields)
-        print('Now going to read {} files (Index: {})'.format(fields, index))
-        path = os.path.join(train_path, fields, '*g')
-        files = glob.glob(path)
-        for i, fl in enumerate(files):
-            if (i + 1) % 100 == 0 or (i + 1) == len(files):
-                print("\t@ image " + str(i + 1) + "/" + str(len(files)))
-            image = read_image(filename=fl, image_size=image_size, num_channels=num_channels)
-            images[k] = image
-            k += 1
-            label = np.zeros(len(classes))
-            label[index] = 1.0
-            labels.append(label)
-            flbase = os.path.basename(fl)
-            img_names.append(flbase)
-            cls.append(fields)
-            if shorter != 0 and i + 1 >= shorter:
-                break
-    # Even if we want a single channel, we have to add a dimension to the array (dimension 1)
-    if num_channels == 1:
-        images = np.expand_dims(images, axis=3)
-    labels = np.array(labels)
-    print("\nImages array of shape : " + str(np.shape(images)))
-    img_names = np.array(img_names)
-    cls = np.array(cls)
-
-    return images, labels, img_names, cls
-
 
 class DataSet(object):
     def __init__(self, images, labels, img_names, cls):
@@ -125,8 +66,63 @@ class DataSet(object):
         return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
 
 
+def read_image(filename, image_size):
+    """ Open a file, read the image and convert it to a optimum shape"""
+    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    image = np.where(image > 30, 1, 0)  # Improve the contrast of the dataset and transform 255 range to 0/1 values
+    image = image.astype(np.bool)
+    return (image)
+
+
+def load_train(train_path, image_size, classes, shorter=0, num_channels=1):
+    """ Load the train dataset in memory """
+    num_images = 0
+    for fields in classes:
+        path = os.path.join(train_path, fields, '*g')
+        files = glob.glob(path)
+        if shorter == 0:
+            num_images += len(files)
+        else:
+            if len(files) < shorter:
+                num_images += len(files)
+            else:
+                num_images += shorter
+    print("\tTotal images number : " + str(num_images))
+    images = np.zeros((num_images, image_size, image_size), dtype=np.bool)
+    labels = np.zeros((num_images, len(classes)), dtype=np.bool)
+    img_names = np.empty((num_images), dtype=object)
+    cls = np.empty((num_images), dtype=object)
+    print('\tGoing to read training images')
+    k = 0
+    for fields in classes:
+        index = classes.index(fields)
+        path = os.path.join(train_path, fields, '*g')
+        if shorter != 0:
+            files = glob.glob(path)[:shorter]
+        else:
+            files = glob.glob(path) 
+        print('\tNow going to read {} files (Index: {}, Num: {})'.format(fields, index, len(files)))
+        for i, fl in enumerate(files):
+            if (i + 1) % 10 == 0 or (i + 1) == len(files):
+                print("\t@ image " + str(i + 1) + "/" + str(len(files)) + " "*5, end="\r")
+            images[k] = read_image(filename=fl, image_size=image_size)
+            labels[k][index] = True
+            img_names[k] = os.path.basename(fl)
+            cls[k] = fields
+            k += 1
+        print("\n\t" + len(files) + " files loaded -> " + fields + " completed" + " "*10)
+    # Even if we want a single channel, we have to add a dimension to the array (dimension 1)
+    if num_channels == 1:
+        images = np.expand_dims(images, axis=3)
+    print("\nImages array of shape : " + str(np.shape(images)))
+    
+    return images, labels, img_names, cls
+
+
 def read_train_sets(train_path, image_size, classes, validation_size, shorter=0, num_channels=1,
                     dataset_save_dir_path=""):
+    """ An already used dataset is saved under .npy files for faster use. We use those files if they exists.
+        Else, we just reload the whole dataset and save it for later use. """
     class DataSets(object):
         pass
 
@@ -146,7 +142,7 @@ def read_train_sets(train_path, image_size, classes, validation_size, shorter=0,
         elif not os.path.isfile(arrayPath):
             reload_data = True
     if not reload_data:
-        print("Try to load npy data in the memory...")
+        print("Try to load npy data in the memory from " + str(dataset_save_dir_path))
         try:
             images = np.load(lesArrayPath[0])
             labels = np.load(lesArrayPath[1])
@@ -158,9 +154,10 @@ def read_train_sets(train_path, image_size, classes, validation_size, shorter=0,
         except:
             reload_data = True
     if reload_data:
-        print("Reloading the dataset...")
+        print("Loading the dataset " + str(train_path))
         images, labels, img_names, cls = load_train(train_path, image_size, classes, shorter=shorter,
                                                     num_channels=num_channels)
+        print("Shuffling")
         images, labels, img_names, cls = shuffle(images, labels, img_names, cls)
         print("\nTry to write the dataset into the folder" + dataset_save_dir_path)
         try:
@@ -169,6 +166,9 @@ def read_train_sets(train_path, image_size, classes, validation_size, shorter=0,
             np.save(lesArrayPath[1], labels)
             np.save(lesArrayPath[2], img_names)
             np.save(lesArrayPath[3], cls)
+            with open(DATASET_SAVE_DIR_PATH + "/labels.txt", "w") as f:
+                for classe in classes:
+                    f.write(classe + "\n")
             print("Done")
         except:
             print("Problem writing the dataset")
